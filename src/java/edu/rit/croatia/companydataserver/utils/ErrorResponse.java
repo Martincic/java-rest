17a51bdbcc703d191d6e4529f2a34b61ef5bd093
tmartincic <tm4818@rit.edu>
2021-11-03 22:36:09 +0100

    Error handler

---
+++ src/java/edu/rit/croatia/companydataserver/utils/ErrorResponse.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.rit.croatia.companydataserver.utils;
+
+/**
+ *
+ * @author Tomas
+ */
+public class ErrorResponse {
+    private int line;
+    
+    public ErrorResponse(int line) {
+        this.line = line;
+    }
+    
+}
